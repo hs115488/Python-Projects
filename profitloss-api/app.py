be61f8e5e0802a057b03a1efd63a0fd0b59cdf3a
Haresh Singh <haresh.singh.97@gmail.com>
2021-07-18 23:15:35 -0400

    Added POST request for adding an entry

--- profitloss-api/app.py
+++ profitloss-api/app.py
@@ -2,7 +2,7 @@ import flask
 import db_connect
 import simplejson
 from db_connect import database
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -16,6 +16,20 @@ def getAllEntries():
     results = cursor.fetchall()
     return simplejson.dumps(results)
 
+@app.route("/addProfitEntry", methods = ["POST"])
+def addEntry():
+    try:
+        trader_name = request.args.get('name')
+        profit_amount = request.args.get('profit')
+
+        connection = database()
+        cursor = database.connect()
+
+        cursor.execute(f"INSERT INTO profits VALUES (DEFAULT, {trader_name}, {profit_amount})")
+        cursor.execute("COMMIT")
+        return "Data Added!"
+    except:
+        print("error")
 
 if __name__ == "__main__":
     app.run()
